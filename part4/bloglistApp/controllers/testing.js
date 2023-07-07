b620ff39a69148e32f611be830802935f5feaf81
Nikolay Iliev <nikolay.iliev@aalto.fi>
2023-07-07 15:51:36 +0000

    exercises 5.17-5.23

---
+++ part4/bloglistApp/controllers/testing.js
@@ -0,0 +1,12 @@
+const testingRouter = require('express').Router()
+const Note = require('../models/blog.js')
+const User = require('../models/user.js')
+
+testingRouter.post('/reset', async (request, response) => {
+  await Note.deleteMany({})
+  await User.deleteMany({})
+
+  response.status(204).end()
+})
+
+module.exports = testingRouter
