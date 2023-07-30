b266e76b28302b6dff8938221594c66160a8f76a
ilievn1 <122593669+ilievn1@users.noreply.github.com>
2023-07-30 14:31:01 +0300

    exercises 13.5-13.16

--- index.js
+++ index.js
@@ -1,90 +1,35 @@
-require("dotenv").config();
-const { Sequelize, Model, QueryTypes, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
+require("express-async-errors");
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
+const { errorHandler, unknownEndpoint } = require("./util/middleware");
+const middleware = require("./util/middleware.js");
+
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const authorsRouter = require("./controllers/authors");
+
+
 app.use(express.json());
+app.use(middleware.requestLogger);
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/authors", authorsRouter);
 
-// const main = async () => {
-//   try {
-//     const blogs = await sequelize.query("SELECT * FROM blogs", {
-//       type: QueryTypes.SELECT,
-//     });
-//     console.log("Executing (default): SELECT * FROM blogs");
-//     blogs.map((blog) =>
-//       console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
-//     );
-//     sequelize.close();
-//   } catch (error) {
-//     console.error("Unable to connect to the database:", error);
-//   }
-// };
+app.use(middleware.tokenExtractor);
+app.use("/api/blogs",blogsRouter);
 
-// main();
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-class Blog extends Model {}
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.TEXT,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  }
-);
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-Blog.sync();
-
-app.get("/", async (req, res) => {
-  res.send('<p>Hello, welcome to the server</p>');
-});
-
-app.get("/api/blogs", async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.json(blogs);
-});
-
-app.post("/api/blogs", async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body);
-    return res.json(blog);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
-});
-
-app.delete("/api/blogs/:id", async (req, res) => {
-  const deletedRows = await Blog.destroy({ where: { id: req.params.id } });
-  if (deletedRows === 1) {
-    res.status(204).end();
-  } else {
-    res.status(404).end();
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start();
